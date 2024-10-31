0dc36c06fbb08b4d8296e6d9f16f82401b2b4615
LeHuy <quanghuymang@gmail.com>
2024-10-31 10:30:23 +0700

    Add project files.

---
+++ WebApViet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApViet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
